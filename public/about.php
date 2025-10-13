2505bee2f6df6b3be443e987947c61ad4be449ba
Oyedokun Kehinde <oyedokunken@gmail.com>
2025-10-14 00:47:18 +0100

    Project Scaffolding Complete

---
+++ public/about.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title> Medicate – Hospital Management System </title>
+   <!-- Favicon Icon -->
+   <link rel="shortcut icon" href="assets/images/favicon.ico">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+   <!-- Fonts and Icons -->
+   <link rel="stylesheet" type="text/css" href="assets/rev/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+   <link rel="stylesheet" type="text/css" href="assets/rev/fonts/font-awesome/css/font-awesome.css">
+   <!-- REVOLUTION STYLE SHEETS -->
+   <link rel="stylesheet" type="text/css" href="assets/rev/css/rs6.css">
+   <!-- Owl Carousel CSS -->
+   <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+   <!-- Progressbar CSS -->
+   <link rel="stylesheet" href="assets/css/progressbar.css">
+   <!-- Animation CSS -->
+   <link rel="stylesheet" href="assets/css/animations.min.css">
+   <!-- Magnific Popup CSS -->
+   <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+   <!-- Icons CSS -->
+   <link rel="stylesheet" href="assets/fonts/font-awesome/css/all.min.css">
+   <link rel="stylesheet" href="assets/fonts/flaticon/flaticon.css">
+   <link rel="stylesheet" href="assets/css/ionicons.min.css">
+   <link rel="stylesheet" href="assets/fonts/themify-icons/themify-icons.css">
+   <!-- Main Style CSS -->
+   <link rel="stylesheet" href="assets/css/style.css">
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+   <!-- Loading Start -->
+   <div id="pq-loading">
+      <div id="pq-loading-center">
+         <img src="assets/images/logo.png" class="img-fluid" alt="loading">
+      </div>
+   </div>
+   <!-- Loading End -->
+
+   <!-- Header Start -->
+   <header id="pq-header" class="pq-header-default ">
+      <div class="pq-top-header">
+         <div class="container">
+            <div class="row flex-row-reverse">
+               <div class="col-md-6 text-right">
+                  <div class="pq-header-social text-right">
+                     <ul>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div class="pq-header-contact ">
+                     <ul>
+                        <li>
+                           <a href="tel:+1800001658"><i class="fas fa-phone"></i>
+                              <span> +1800-001-658</span>
+                           </a>
+                        </li>
+                        <li>
+                           <a href="mailto:info@peacefulthemes.com"><i class="fas fa-envelope"></i><span>info@peacefulthemes.com</span></a>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="pq-bottom-header pq-has-sticky">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-12">
+                  <nav class="navbar navbar-expand-lg navbar-light">
+                     <a class="navbar-brand" href="index.php">
+                        <img class="img-fluid logo" src="assets/images/logo.png" alt="medicate">
+                     </a>
+                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div id="pq-menu-contain" class="pq-menu-contain">
+                           <ul id="pq-main-menu" class="navbar-nav ml-auto">
+                              <li class="menu-item current-menu-item">
+                                 <a href="index.php">Home</a>
+                              </li>
+                              <li class="menu-item">
+                                 <a href="about-us.html">About Us</a>
+                              </li>
+                              <li class="menu-item">
+                                 <a href="#">Services</a>
+                                 <ul class="sub-menu">
+                                    <li><a href="angioplasty-services.html">Angioplasty Services</a></li>
+                                    <li><a href="cardiology-services.html">Cardiology Services</a></li>
+                                    <li><a href="dental-services.html">Dental Services</a></li>
+                                    <li><a href="endocrinology-services.html">Endocrinology Services</a></li>
+                                    <li><a href="eye-care-services.html">Eye Care Services</a></li>
+                                    <li><a href="nurology-service.html">Neurology Services</a></li>
+                                    <li><a href="orthopaedics-services.html">Orthopaedics Services</a></li>
+                                    <li><a href="rmi-services.html">RMI Services</a></li>
+                                 </ul>
+                              </li>
+                              <li class="menu-item"><a href="blog.html">Blog</a></li>
+                              <li class="menu-item"><a href="contact-us.html">Contact Us</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                     <a href="make-appoinment.html" class="pq-button">
+                        <div class="pq-button-block">
+                           <span class="pq-button-text">Appointments</span>
+                           <i class="ion ion-plus-round"></i>
+                        </div>
+                     </a>
+                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-bars"></i>
+                     </button>
+                  </nav>
+               </div>
+            </div>
+         </div>
+      </div>
+   </header>
+   <!-- Header End -->
+
+   <!-- REST OF YOUR HTML CONTENT GOES HERE -->
+   <!-- (Keep everything from <rs-module-wrap> down to </body>) -->
+   <!-- Just make sure ALL image paths use "assets/..." -->
+
+   <!-- Example: -->
+   <!-- <img src="assets/images/About-us/1.jpg" ...> -->
+   <!-- <img src="assets/rev/assets/1-51.jpg" ...> -->
+
+   <!-- Footer & Scripts -->
+   <footer id="pq-footer">
+      <!-- ... your footer content ... -->
+   </footer>
+
+   <!-- Back To Top -->
+   <div id="back-to-top">
+      <a class="topbtn" id="top" href="#top"> <i class="ion-ios-arrow-up"></i> </a>
+   </div>
+
+   <!-- JS Files -->
+   <script src="assets/js/jquery.min.js"></script>
+   <script src="assets/js/bootstrap.min.js"></script>
+   <script src="assets/js/owl.carousel.min.js"></script>
+   <script src="assets/js/progressbar.js"></script>
+   <script src="assets/js/isotope.pkgd.min.js"></script>
+   <script src="assets/js/jquery.countTo.min.js"></script>
+   <script src="assets/js/jquery.magnific-popup.min.js"></script>
+   <script src="assets/js/wow.min.js"></script>
+   <script src="assets/rev/js/rbtools.min.js"></script>
+   <script src="assets/rev/js/rs6.min.js"></script>
+   <script src="assets/js/rev-custom.js"></script>
+   <script src="assets/js/custom.js"></script>
+
+   <!-- Keep your inline script if needed -->
+   <script>
+      // Your existing form script
+   </script>
+</body>
+</html>
--- public/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '../config/config.php';
-require_once '../includes/header.php';
-?>
-<h1>Medicate Setup Successful!</h1>
-<?php require_once '../includes/footer.php'; ?>
